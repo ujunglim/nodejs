f6ca156c467ea383a0ec3deb11fb6fc9de0b24ca
v_linyouzhen <v_linyouzhen@baidu.com>
2022-09-08 13:52:33 +0800

    Express로 html보내기

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const http = require('http');
 const fs = require('fs');
+const express = require('express');
+const server = express();
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        fs.readFile('./index.html', null, (err, data) => {
-            // 서버에서 어떤파일을 보내는지 알려줘야함
-            res.writeHead(200, {
-               'Content-Type': 'text/html' 
-            })
-            res.write(data);
-            res.end(); // res 끝났을떄
-        })
-    }
-});
+// http는 url 하나하나마다 써야될게 많은데 express는 간단
+server.get('/', (req, res) => {
+    res.statusCode(200).sendFile(__dirname + '/index.html');
+})
+
+// http server
+// const server = http.createServer((req, res) => {
+//     if(req.url === '/') {
+//         // 파일을 하나하나 불러와야함
+//         fs.readFile('./index.html', null, (err, data) => {
+//             // 서버에서 어떤파일을 보내는지 알려줘야함
+//             res.writeHead(200, {
+//                'Content-Type': 'text/html' 
+//             })
+//             res.write(data);
+//             res.end(); // res 끝났을떄
+//         })
+//     }
+// });
 
 server.listen(3000, (err) => {
     if (err) throw err;
