66534e805d05b163c59aac0ece121388f5b7e4af
v_linyouzhen <v_linyouzhen@baidu.com>
2022-09-08 13:39:49 +0800

    fs를 통해서 서버에서 html받기

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/') {
-        res.write('hi from server');
-        res.end();
+        fs.readFile('./index.html', null, (err, data) => {
+            // 서버에서 어떤파일을 보내는지 알려줘야함
+            res.writeHead(200, {
+               'Content-Type': 'text/html' 
+            })
+            res.write(data);
+            res.end(); // res 끝났을떄
+        })
     }
 });
 
