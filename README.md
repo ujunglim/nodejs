aa9b3445145ad0f61585c2f133e5eed0d8b2f3c4
Yuyu <ujunglim@naver.com>
2022-09-09 09:23:32 +0800

    template handlebar

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ nodemon 파일이름
 이미지 저장
 html에 이미지 부르기
 server.use(express.static())으로 이미지가 어딨는지 알려주기
+
+# 5. Template Engine
+
+emplate Engine을 통하여서 반복적으로 html을 생성하는데신 하나의 html에서 데이터만 바꿀수 있다.
+npm i express-handlebars
+partialsDir: 반복적인 코드를 쉐어링할수 있게 경로설정
+템플릿 파일들은 모두 views폴더 안에 넣기 (노드가 템플릿을 사용할때 자동으로 views폴더 안을 찾기 때문)
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const http = require("http");
 const fs = require("fs");
 const express = require("express");
+const hbs = require("express-handlebars");
 const server = express();
+
+// template
+server.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+
+server.set("view engine", "hbs");
 
 server.use(express.static(__dirname + "/public"));
 server.use((req, res, next) => {
@@ -10,7 +24,8 @@ server.use((req, res, next) => {
 
 // http는 url 하나하나마다 써야될게 많은데 express는 간단
 server.get("/", (req, res) => {
-  res.status(200).sendFile(__dirname + "/index.html");
+  //   res.status(200).sendFile(__dirname + "/index.html");
+  res.status(200).render("index"); // index.hbs
 });
 
 // http server
